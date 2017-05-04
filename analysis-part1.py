72f6a59f00e73609448b2e49005a21370382df83
aredev <appierasool@gmail.com>
2017-05-04 16:19:44 +0200

    Beetje verder

--- analysis-part1.py
+++ analysis-part1.py
@@ -4,6 +4,8 @@ import scipy.stats
 
 __author__ = "Tom Sandmann (s4330048) & Abdullah Rasool (s4350693)"
 
+#correct key 12
+
 # Present sbox
 sbox = dict([
     (0, 12),
@@ -40,16 +42,13 @@ def apply_selection_function(messages, traces):
     values_one = numpy.zeros([len(messages), 4])
     idx_one = [0, 0, 0, 0]
 
-    skewness_zero = [0, 0, 0, 0]
-    skewness_one = [0, 0, 0, 0]
-
     for i, m in enumerate(messages):
         for k in keys:
             mbin = format(m[0], "04b")
             kbin = format(k, "04b")
 
-            selection_value = (int(mbin[1]) + int(kbin[1])) * (int(mbin[2]) + int(kbin[2])) + int(mbin[2]) + int(
-                mbin[3])
+            selection_value = (int(mbin[3]) + int(kbin[3])) * (int(mbin[2]) + int(kbin[2])) + int(mbin[2]) + int(
+                mbin[1])
             selection_value = selection_value % 2
 
             if selection_value == 0:
@@ -58,12 +57,6 @@ def apply_selection_function(messages, traces):
             else:
                 values_one[idx_one[k]][k] = traces[i][0]
                 idx_one[k] += 1
-
-            skewness_zero[k] = scipy.stats.skew(values_zero[:, k])
-            skewness_one[k] = scipy.stats.skew(values_one[:, k])
-
-    print(skewness_one)
-    print(skewness_zero)
 
     return values_zero, values_one
 
@@ -120,27 +113,8 @@ def selection_function(messages, traces):
     results = []
 
     for i in range(0, 4):
+        # Correct key should be 2
         key_i_zero_traces = zero_values[:, i] #Get column of key with value i
-        key_i_zero_traces = numpy.asarray(list(filter(lambda a: a != 0, key_i_zero_traces)))
-
-        key_i_one_traces = one_values[:, i]
-        key_i_one_traces = numpy.asarray(list(filter(lambda a: a != 0, key_i_one_traces)))
-
-        skewness_zero = scipy.stats.skew(key_i_zero_traces)
-        skewness_one = scipy.stats.skew(key_i_one_traces)
-        skewness_delta = abs(skewness_one - skewness_zero)
-
-        results.append((skewness_delta, i))
-
-    results = sorted(results, key=lambda tup: tup[1])
-    print(results)
-
-    zero_values, one_values = apply_second_selection_function(messages, traces)
-    results = []
-
-    for i in range(0, 4):
-        key_i_zero_traces = zero_values[:, i]  # Get column of key with value i
-        print(key_i_zero_traces)
 
         key_i_one_traces = one_values[:, i]
 
@@ -150,12 +124,28 @@ def selection_function(messages, traces):
 
         results.append((skewness_delta, i))
 
-    results = sorted(results, key=lambda tup: tup[1])
+    results = sorted(results, key=lambda tup: tup[1], reverse=True)
     print(results)
 
 
 
-
+    # zero_values, one_values = apply_second_selection_function(messages, traces)
+    # results = []
+    #
+    # for i in range(0, 4):
+    #     key_i_zero_traces = zero_values[:, i]  # Get column of key with value i
+    #     print(key_i_zero_traces)
+    #
+    #     key_i_one_traces = one_values[:, i]
+    #
+    #     skewness_zero = scipy.stats.skew(key_i_zero_traces)
+    #     skewness_one = scipy.stats.skew(key_i_one_traces)
+    #     skewness_delta = abs(skewness_one - skewness_zero)
+    #
+    #     results.append((skewness_delta, i))
+    #
+    # results = sorted(results, key=lambda tup: tup[1])
+    # print(results)
 
 messages, traces = read_data()
 selection_function(messages, traces)
